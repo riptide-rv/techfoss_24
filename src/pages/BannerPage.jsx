fbe51cce99ab995ec249a5bc5ea65cc442330eb7
Rohit Ramdas V <rohitramdas2003@gmail.com>
2024-02-23 23:42:29 +0530

    more refinement glitch bug

--- src/pages/BannerPage.jsx
+++ src/pages/BannerPage.jsx
@@ -12,20 +12,25 @@ export function BannerPage(){
                     <div className='w-[100%] h-[45%] flex justify-center items-center'>
                         <section className='text-9xl cyberpunk2077 flex  pr-6 justify-center w-[60%] h-[60%] items-center'
                         style={{
-                            "text-shadow": "-3px 0 aqua, 0 1px yellow, 1px 0 yellow, 0 -1px yellow",
+                            
                             "color":"#ffee08",
                             "cursor":"default",
                             "shadow" : "0 0 10px #ffee08, 0 0 20px #ffee08, 0 0 40px #ffee08, 0 0 80px #ffee08",
                                                 
                         }} 
                         >
-                            <div className='p-12 pr-6 pl-16 glitch'>
+                            <div className='p-12 pr-6 pl-16 '>
                                 <img src={logo} alt="TECHFOSS"/>
                             </div>
                             <div>
-                        <h6 className='glitch  sm:mr-4 mr-6 sm:text-8xl text-5xl' data-text="TECHFOSS">TECHFOSS</h6> 
+                        <h6 className='glitch  sm:mr-4 mr-6 sm:text-8xl text-5xl' style={{
+                            "text-shadow": "-3px 0 aqua, 0 1px yellow, 1px 0 yellow, 0 -1px yellow",
+                            "user-select":"none",
+                            "-webkit-user-select":"none",
+                            "-ms-user-select":"none",
+                        }} data-text="TECHFOSS">TECHFOSS</h6> 
                         <h2 style={{
-                             "text-shadow": "0px 0 aqua, 0 1px aqua, 1px 0 yellow, 0 -1px yellow",
+                             "text-shadow": "0px 0 aqua, 0 0px aqua, 1px 0 yellow, 0 -1px yellow",
                              "color":"#ffee08",
                              "cursor":"default",
                              "letterSpacing": "0.09rem",
