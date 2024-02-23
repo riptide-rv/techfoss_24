415c657f93a995d6b8b829412e8152e739202f98
Rohit Ramdas V <rohitramdas2003@gmail.com>
2024-02-24 00:18:25 +0530

    adding dept logos

--- src/pages/BannerPage.jsx
+++ src/pages/BannerPage.jsx
@@ -2,12 +2,16 @@ import './bannerpage.scss';
 import {About} from './About'
 import { Navbar } from './Navbar';
 import logo from '../images/techfossloggo.png';
+import csi from '../images/csi.png';
+import toch from '../images/toch.png';
+import deptcs from '../images/deptcs.png';
+
 
 export function BannerPage(){
     return(
-        <div className="w-[100vw] h-[230vh] banner_bg ">
+        <div className="w-[100vw] h-[230vh] banner_bg bg-[#090909] ">
            
-            <div className='w-[100%] h-[100%] banner_overlay' id="home">
+            <div className='w-[100%] h-[90%]  banner_overlay' id="home">
           
                     <div className='w-[100%] h-[45%] flex justify-center items-center'>
                         <section className='text-9xl cyberpunk2077 flex  pr-6 justify-center w-[60%] h-[60%] items-center'
@@ -38,6 +42,18 @@ export function BannerPage(){
                         }} className='sub-text text-base xs:text-xl sm:text-xl md:text-3xl lg:text-3xl xl:text-3xl 2xl:text-3xl'>FEBRUARY 27, 2024</h2>
                             </div>
                         </section>
+                        
+                    </div>
+                    <div className='h-[10px] w-[100%]'>
+                        <div className='flex pr-8 pl-3 text-black justify-between items-between'>
+                            <div>
+                                <div><img src={toch} className='sm:h-10 h-6' alt="tochlogo" /></div>
+                            </div>
+                            <div className='flex'>
+                                <div><img src={csi} className='sm:h-10 h-7 ml-3 mr-2' alt="csilogo" /></div>
+                                <div> <img src={deptcs} alt="deptcslogo"  className='sm:h-10 h-8 sm:mr-5 sm:ml-5'/> </div>
+                            </div>
+                        </div>
                     </div>
                 <div className='relative'>
                     <About/>
